88f0a50874d856c10e778b043f1afd9858dfbc14
Emre Değirmenci <emred1999@hotmail.com>
2022-01-26 22:20:00 +0300

    Bugs fixed and database connection established.

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -10,16 +10,55 @@ namespace Business.Concrete
 {
     public class CarManager : ICarService
     {
-        ICarDal _iCarDal;
+        ICarDal _carDal;
 
-        public CarManager(ICarDal iCarDal)
+        public CarManager(ICarDal carDal)
         {
-            _iCarDal = iCarDal;
+            _carDal = carDal;
+        }
+
+        public void Add(Car car)
+        {
+            if (car.Description.Length < 2)
+            {
+                Console.WriteLine("Car name must be at least 2 characters");
+            }
+            else if (car.DailyPrice <= 0)
+            {
+                Console.WriteLine("Car daily price must be greater than 0.");
+            }
+            else
+            {
+                _carDal.Add(car);
+                Console.WriteLine("Car Added!");
+            }
+        }
+
+        public void Delete(Car car)
+        {
+            _carDal.Delete(car);
+            Console.WriteLine("Car deleted!");
         }
 
         public List<Car> GetAll()
         {
-            return _iCarDal.GetAll();
+            return _carDal.GetAll();
+        }
+
+        public List<Car> GetCarsByBrandId(int id)
+        {
+            return _carDal.GetAll(c => c.BrandId == id);
+        }
+
+        public List<Car> GetCarsByColorId(int id)
+        {
+            return _carDal.GetAll(c => c.ColorId == id);
+        }
+
+        public void Update(Car car)
+        {
+            _carDal.Update(car);
+            Console.WriteLine("Car updated!");
         }
     }
 }
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,5 +1,8 @@
 ﻿using Business.Concrete;
 using DataAccess.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
 using System;
 
 namespace ConsoleUI
@@ -8,12 +11,41 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            CarManager carManager = new CarManager(new InMemoryCarDal());
-            foreach (var car in carManager.GetAll())
+            CarManager carManager = new CarManager(new EfCarDal());
+            foreach (var car in carManager.GetCarsByBrandId(1))
             {
-                Console.WriteLine(car.Id + " "+ car.Description);
+                Console.WriteLine(car.BrandId + " " + car.Description);
             }
-        }
+            foreach (var car in carManager.GetCarsByColorId(2))
+            {
+                Console.WriteLine(car.ColorId + " " + car.Description);
 
+            }
+
+            Console.WriteLine("********AddCars***********");
+
+            Car car1 = new Car
+            {
+                BrandId = 1,
+                ColorId = 1,
+                ModelYear = 2020,
+                DailyPrice = 0,
+                Description = "Alfa Romeo"
+            };
+
+            carManager.Add(car1);
+
+            Car car2 = new Car
+            {
+                BrandId = 1,
+                ColorId = 1,
+                ModelYear = 2005,
+                DailyPrice = 0,
+                Description = "a"
+            };
+
+            carManager.Add(car2);
+
+        }
     }
 }
